ad6cfd22f1652514ab93efd6cc3c1e3912795846
Georgen1 <123319264+bachurskii@users.noreply.github.com>
2023-07-12 14:01:13 +0300

    a little update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import statics from './statics.json';
 import user from './user.json';
 import friends from './friends.json';
 import transactions from './transactions.json';
-import Statics from 'profile/Statistics/statics';
-import FriendList from 'profile/friendslist/friends';
+import Statics from 'Statistics/statics';
+import FriendList from 'friendslist/friends';
 import Profile from 'profile/profile';
 import TransactionHistory from 'transactionhistory/transaction';
 
