82ce21d229d5f90d682b57765549e07ce8456082
Georgen1 <123319264+bachurskii@users.noreply.github.com>
2023-07-12 14:55:49 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import profile from './components/profile/profile.css';
-import friends from './components/friendslist/friends.css';
-import statics from './components/Statistics/statics.css';
-import transaction from './components/transactionhistory/transaction.css';
+import './components/profile/profile.css';
+import './components/friendslist/friends.css';
+import './components/Statistics/statics.css';
+import './components/transactionhistory/transaction.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
